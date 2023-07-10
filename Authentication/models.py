789a98d845a6125eed08cac840eebdbf728eadb0
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-07-10 16:22:10 +0330

    Connected DataBase & Created first model

--- Authentication/models.py
+++ Authentication/models.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
 
+class User(AbstractUser):
+    id = models.BigAutoField(primary_key=True, null=False, blank=False)
+    username = models.CharField(unique=True, max_length=150)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.username
--- MoneyMate/settings.py
+++ MoneyMate/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Authentication.apps.AuthenticationConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'MoneyMate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'moneymate',
+        'USER': 'mehrad',
+        'PASSWORD': 'mehrad1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -121,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'Authentication.User'
