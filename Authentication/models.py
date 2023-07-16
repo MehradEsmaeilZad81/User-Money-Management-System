d3ff8b4186edae4bc3c1f09c7e2ff170323b3ea3
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-07-16 12:06:06 +0330

    Added User to admin panel

--- Authentication/models.py
+++ Authentication/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
     password = models.CharField(max_length=255)
-    username = None
+    username = models.CharField(max_length=50, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username',]
