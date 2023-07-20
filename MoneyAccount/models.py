3134ac4995d34cb4bc7ff23c46dce915169890ab
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-07-20 12:37:16 +0330

    Created GeneralSource model

--- MoneyAccount/models.py
+++ MoneyAccount/models.py
@@ -2,15 +2,17 @@ from Authentication.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
+import schedule
+import time
 # Create your models here.
 
 
 class MoneyAccount(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     balance = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0.00)
-    income = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+        max_digits=10, decimal_places=2, default=10000.00)
+    income = models.DecimalField(
+        max_digits=10, decimal_places=2, default=10000.00)
     expense = models.DecimalField(
         max_digits=10, decimal_places=2, default=0.00)
 
@@ -40,10 +42,26 @@ class MoneyAccount(models.Model):
         }
 
 
+class GeneralSource(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    inventory = models.DecimalField(
+        max_digits=10, decimal_places=2, default=100000.00)
+    coefficient = models.DecimalField(
+        max_digits=2, decimal_places=2, default=0.50)
+    deposit_interval = models.PositiveIntegerField(default=1)
+    deposit_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=1000.00)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
 @receiver(post_save, sender=User)
 def create_money_account(sender, instance, created, **kwargs):
+    # Create a money account for the newly registered user.
     if created:
         MoneyAccount.objects.create(user=instance)
 
-
+# Connect the signal to the function to automatically create money accounts for new users.
 post_save.connect(create_money_account, sender=User)
--- MoneyMate/settings.py
+++ MoneyMate/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'MoneyMate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -88,7 +89,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
