8ea347e418c43b5cd9087fb3b10033374517cec1
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-08-12 13:21:08 +0330

    Refactor views.py for improved readability and maintainability
    This commit refactors the views.py file to enhance code readability and maintainability.

--- MoneyAccount/views.py
+++ MoneyAccount/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import GeneralSource, Subscription, Transaction, MoneyAccount
-from .serializers import GeneralSourceSerializer, SubscriptionRequestSerializer, SubscriptionSerializer, SubscriptionMoneyRequestSerializer, MoneyAccountSerializer
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from rest_framework import status
 import jwt
-from Authentication.models import User
-from rest_framework.exceptions import AuthenticationFailed
 
-# Create your views here.
+from Authentication.models import User
+from .models import GeneralSource, Subscription, Transaction, MoneyAccount
+from .serializers import (
+    GeneralSourceSerializer,
+    SubscriptionRequestSerializer,
+    SubscriptionSerializer,
+    SubscriptionMoneyRequestSerializer,
+    MoneyAccountSerializer,
+)
 
 
 def IsAuthenticated(request):
@@ -28,6 +33,51 @@ def IsAuthenticated(request):
 
     user = User.objects.filter(id=payload['id']).first()
     return user
+
+
+# Create your views here.
+
+class GeneralSourceView(APIView):
+    def get(self, request):
+        user = IsAuthenticated(request)
+        generalsources = GeneralSource.objects.all()
+        serializer = GeneralSourceSerializer(generalsources, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        user = IsAuthenticated(request)
+        serializer = SubscriptionRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        general_source_name = serializer.validated_data['general_source_name']
+        amount = serializer.validated_data['proposed_amount']
+        general_source = GeneralSource.objects.filter(
+            name=general_source_name).first()
+        money_account = MoneyAccount.objects.filter(user=user).first()
+
+        if Subscription.objects.filter(general_source=general_source, money_account=money_account).exists():
+            return Response({
+                'message': 'Subscription already exists'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        money_account.add_expense(amount)
+        general_source.add_amount(amount)
+        money_account.create_subscription(general_source, amount)
+        money_account.create_transaction(general_source, amount, 'D')
+
+        return Response({
+            'message': 'Subscription created successfully. Waiting for approval.'
+        }, status=status.HTTP_201_CREATED)
+
+
+class MySubscriptionView(APIView):
+    def get(self, request):
+        user = IsAuthenticated(request)
+        money_account = MoneyAccount.objects.filter(user=user).first()
+        subscriptions = Subscription.objects.filter(
+            money_account=money_account)
+        serializer = SubscriptionSerializer(subscriptions, many=True)
+        return Response(serializer.data)
 
 
 class GeneralSourceDetailView(APIView):
@@ -86,50 +136,3 @@ class MoneyAccountView(APIView):
         moneyaccounts = MoneyAccount.objects.all()
         serializer = MoneyAccountSerializer(moneyaccounts, many=True)
         return Response(serializer.data)
-
-
-class GeneralSourceView(APIView):
-    def get(self, request):
-        user = IsAuthenticated(request)
-        generalsources = GeneralSource.objects.all()
-        serializer = GeneralSourceSerializer(generalsources, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        user = IsAuthenticated(request)
-        serializer = SubscriptionRequestSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        general_source_name = serializer.validated_data['general_source_name']
-        amount = serializer.validated_data['proposed_amount']
-        general_source = GeneralSource.objects.filter(
-            name=general_source_name).first()
-        money_account = MoneyAccount.objects.filter(user=user).first()
-
-        if Subscription.objects.filter(general_source=general_source, money_account=money_account).exists():
-            return Response({
-                'message': 'Subscription already exists'
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-        general_source.add_amount(amount)
-        general_source.save()
-
-        money_account.add_expense(amount)
-        money_account.save()
-        # Create the subscription for the logged-in user
-        Subscription.objects.create(
-            money_account=money_account,
-            general_source=general_source,
-            amount=amount,
-        )
-
-        Transaction.objects.create(
-            money_account=money_account,
-            general_source=general_source,
-            amount=amount,
-            transaction_type='D'
-        )
-
-        return Response({
-            'message': 'Subscription created successfully. Waiting for approval.'
-        }, status=status.HTTP_201_CREATED)
