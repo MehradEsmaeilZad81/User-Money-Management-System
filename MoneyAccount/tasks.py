1f033cd0c052b1521f991bf51ff52c8b08d758d0
Mehrad EsmaeilZad <mehrad.es81@gmail.com>
2023-08-13 13:06:21 +0330

    Refactored tasks.py for improved readability and maintainability

--- MoneyAccount/tasks.py
+++ MoneyAccount/tasks.py
@@ -3,6 +3,7 @@ from celery import shared_task
 from .models import MoneyAccount, GeneralSource, Subscription, Transaction
 from django.utils import timezone
 
+
 @shared_task
 def update_general_sources():
     massage = ""
@@ -27,21 +28,16 @@ def update_subscription():
         current_time = timezone.now()
         time_difference = current_time - subscription.last_updated
         time_difference_seconds = time_difference.total_seconds()
-        flag = time_difference_seconds >= (subscription.general_source.deposit_interval*60 - 15)
+        flag = time_difference_seconds >= (
+            subscription.general_source.deposit_interval*60 - 15)
         if flag:
             if subscription.money_account.balance >= subscription.amounts:
-                subscription.money_account.balance -= subscription.amount
-                subscription.money_account.save()
-                subscription.general_source.inventory += subscription.amount
-                subscription.general_source.save()
+                subscription.money_account.add_expense(subscription.amount)
+                subscription.general_source.add_amount(subscription.amount)
                 subscription.last_updated = current_time
                 subscription.save()
-                Transaction.objects.create(
-                    money_account=subscription.money_account,
-                    general_source=subscription.general_source,
-                    amount=subscription.amount,
-                    transaction_type='W'
-                )
+                subscription.money_account.create_transaction(
+                    subscription.general_source, subscription.amount, 'D')
                 massage += f"Subscription for {subscription.money_account.user.email} - {subscription.general_source.name} has been updated!\n"
 
             else:
